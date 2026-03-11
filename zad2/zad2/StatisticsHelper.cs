b2c755e77dd3bbe8f34c58e86e369507f2189235
Karina Szczepanek <s26646@pjwstk.edu.pl>
2026-03-11 13:03:56 +0100

    added class StatisticsHelper

---
+++ zad2/zad2/StatisticsHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace zad2;
+
+public class StatisticsHelper
+{
+    private int a;
+    
+    public StatisticsHelper(int a)
+    {
+        this.a = a;
+    }
+
+    public void Multiply()
+    {
+        a = a * 2;
+        Console.WriteLine($"a*2={a}");
+    }
+}
